663c17149cf6f48adba967e8380eb0d02fc75a19
student myportfolio <kritikumari75@gmail.com>
2025-07-27 21:45:38 +0530

    updated code in todo list

--- src/components/header.js
+++ src/components/header.js
@@ -1,3 +1,105 @@
+// import React from "react";
+// import { Link } from "react-router-dom";
+
+// function Header(props) {
+//   return (
+//     <header>
+//       <nav
+//         className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}
+//       >
+//         <div className="container-fluid">
+//           <Link className="navbar-brand" to="/">
+//             My Portfolio
+//           </Link>
+
+//           <div className="collapse navbar-collapse" id="navbarSupportedContent">
+//             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+//               <li className="nav-item">
+//                 <Link className="nav-link active" aria-current="page" to="/">
+//                   Home
+//                 </Link>
+//               </li>
+//               <li className="nav-item">
+//                 <Link
+//                   className="nav-link active "
+//                   aria-current="page"
+//                   to="/About"
+//                 >
+//                   About
+//                 </Link>
+//               </li>
+
+//               <li className="nav-item dropdown active" aria-current="page">
+//                 <Link
+//                   className="nav-link dropdown-toggle"
+//                   to="/Projects"
+//                   role="button"
+//                   data-bs-toggle="dropdown"
+//                   aria-expanded="false"
+//                   aria-current="page"
+//                 >
+//                   Projects
+//                 </Link>
+//                 <ul className="dropdown-menu">
+//                   <li className="dropdown-item">
+//                     <Link to="/Headlines">newsapp</Link>
+//                   </li>
+
+//                   <li className="dropdown-item">
+//                     <Link to="/ToDoList">To-Do List</Link>{" "}
+//                   </li>
+
+//                   <li className="dropdown-item">
+//                     <Link to="/weatherApp">weather App</Link>{" "}
+//                   </li>
+
+//                   <li className="dropdown-item">
+//                     <Link to="/QRcode">QRcode</Link>{" "}
+//                   </li>
+//                 </ul>
+//               </li>
+//             </ul>
+
+//             <button
+//               className="navbar-toggler"
+//               type="button"
+//               data-bs-toggle="collapse"
+//               data-bs-target="#navbarSupportedContent"
+//               aria-controls="navbarSupportedContent"
+//               aria-expanded="false"
+//               aria-label="Toggle navigation"
+//             >
+//               <span className="navbar-toggler-icon"></span>
+//             </button>
+
+//             <div
+//               className={`form-check form-switch text-${
+//                 props.mode === "light" ? "Dark" : "light"
+//               }`}
+//             >
+//               <input
+//                 className="form-check-input"
+//                 onClick={props.toggleMode}
+//                 type="checkbox"
+//                 role="switch"
+//                 id="flexSwitchCheckDefault"
+//               />
+//               <label
+//                 className="form-check-label"
+//                 htmlFor="flexSwitchCheckDefault"
+//               >
+//                 Enable Dark Mode
+//               </label>
+//             </div>
+//           </div>
+//         </div>
+//       </nav>
+//     </header>
+//   );
+// }
+// export default Header;
+
+
 import React from "react";
 import { Link } from "react-router-dom";
 
@@ -6,12 +108,27 @@ function Header(props) {
     <header>
       <nav
         className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}
+        
       >
         <div className="container-fluid">
           <Link className="navbar-brand" to="/">
             My Portfolio
           </Link>
 
+          {/* Mobile Toggle Button */}
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          {/* Collapsible Content */}
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
@@ -20,61 +137,67 @@ function Header(props) {
                 </Link>
               </li>
               <li className="nav-item">
-                <Link
-                  className="nav-link active "
-                  aria-current="page"
-                  to="/About"
-                >
+                <Link className="nav-link active" to="/About">
                   About
                 </Link>
               </li>
 
-              <li className="nav-item dropdown active" aria-current="page">
+              {/* Dropdown Menu */}
+              <li className="nav-item dropdown">
                 <Link
                   className="nav-link dropdown-toggle"
-                  to="/Projects"
+                  to="#"
                   role="button"
                   data-bs-toggle="dropdown"
                   aria-expanded="false"
-                  aria-current="page"
                 >
                   Projects
                 </Link>
-                <ul className="dropdown-menu">
-                  <li className="dropdown-item">
-                    <Link to="/Headlines">newsapp</Link>
+                {/* <ul className="dropdown-menu">
+                  <li>
+                    <Link className="dropdown-item" to="/Headlines">
+                      NewsApp
+                    </Link>
                   </li>
-
-                  <li className="dropdown-item">
-                    <Link to="/ToDoList">To-Do List</Link>{" "}
+                  <li>
+                    <Link className="dropdown-item" to="/ToDoList">
+                      To-Do List
+                    </Link>
                   </li>
-
-                  <li className="dropdown-item">
-                    <Link to="/weatherApp">weather App</Link>{" "}
+                  <li>
+                    <Link className="dropdown-item" to="/weatherApp">
+                      Weather App
+                    </Link>
                   </li>
+                  <li>
+                    <Link className="dropdown-item" to="/QRcode">
+                      QR Code
+                    </Link> */}
 
-                  <li className="dropdown-item">
-                    <Link to="/QRcode">QRcode</Link>{" "}
-                  </li>
-                </ul>
-              </li>
-            </ul>
+                    <ul className="dropdown-menu">
+                   <li className="dropdown-item">
+                     <Link to="/Headlines">newsapp</Link>
+                   </li>
 
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarSupportedContent"
-              aria-controls="navbarSupportedContent"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
+                   <li className="dropdown-item">
+                     <Link to="/ToDoList">To-Do List</Link>{" "}
+                   </li>
 
+                   <li className="dropdown-item">
+                     <Link to="/weatherApp">weather App</Link>{" "}
+                   </li>
+
+                   <li className="dropdown-item">
+                     <Link to="/QRcode">QRcode</Link>{" "}
+                   </li>
+                 </ul>
+               </li>
+             </ul>
+                  
+            {/* Dark Mode Toggle */}
             <div
               className={`form-check form-switch text-${
-                props.mode === "light" ? "Dark" : "light"
+                props.mode === "light" ? "dark" : "light"
               }`}
             >
               <input
@@ -97,6 +220,5 @@ function Header(props) {
     </header>
   );
 }
+
 export default Header;
-
-
